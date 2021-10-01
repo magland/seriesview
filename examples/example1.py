ae15963a332f3d74bd44121a37fadc9b900ce77a
Jeremy Magland <jeremy.magland@gmail.com>
2021-10-01 10:20:05 -0400

    add example

--- examples/example1.py
+++ examples/example1.py
@@ -2,19 +2,26 @@ import numpy as np
 import seriesview as sev
 
 def main():
-    x = np.arange(0, 100, dtype=np.float32) / 100
-    y1 = x
-    y2 = x ** 2
-    y3 = x ** 3
-    ts: sev.Timeseries = sev.Timeseries.from_numpy(
-        channel_names=['1', '2', '3'],
+    n = 100
+    x = np.arange(0, n, dtype=np.float32) / n
+    y = x ** 2
+    X = sev.SVSeries.from_numpy(
+        type='continuous',
+        sampling_frequency=1/n,
+        start_time=0,
+        end_time=1,
+        segment_duration=0.3,
         timestamps=x,
-        values=np.stack([y1, y2, y3]).T,
-        type='continuous'
+        values=y
     )
-    F = ts.figurl()
-    url = F.url(label='Test seriesview')
-    print(url)
+    Y = sev.SVSeries.from_uri(X.to_uri())
+    t, v = Y.get_samples(start=0.1, end=0.5)
+    print(Y.to_dict())
+    print(t)
+    print(v)
+    # F = ts.figurl()
+    # url = F.url(label='Test seriesview')
+    # print(url)
 
 if __name__ == '__main__':
     main()
